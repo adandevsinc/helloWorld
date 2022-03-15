18514ebf0f16f854f202db112c8493369e976305
adandevsinc <adan.shahzad@devsinc.com>
2022-03-16 04:17:11 +0500

    arrays in ruby

---
+++ arrays.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+array = [1, 2, 3, 'four', 5.0, true, 7.0]
+
+array[0] = 0
+puts array[0]
+puts array[1]
+puts array[-1]
+
+puts "\n\n"
+puts array[2, 5]
+
+puts "\n\n"
+puts array[2..-1]
+
+puts "\n\n"
+puts array.length
