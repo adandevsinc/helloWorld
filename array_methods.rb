0594739b0c63534eea1d4e8476d38dd6950cd719
adandevsinc <adan.shahzad@devsinc.com>
2022-03-16 04:34:19 +0500

    exploring array methods

---
+++ array_methods.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+list = []
+
+list.push('Tayyab')
+list.push('Adan')
+list.push('Fahad')
+
+puts list
+puts "\n"
+
+puts list.reverse
+puts "\n"
+
+puts list.sort
+puts "\n"
+
+puts list.pop
+puts "\n"
+
+puts list
+puts "\n"
+
+puts list.length
