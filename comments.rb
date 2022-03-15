b8d91e6c88f83565ed2e1d714cb3da35f14fd84e
adandevsinc <adan.shahzad@devsinc.com>
2022-03-16 01:49:55 +0500

    different methods of comments

---
+++ comments.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# single line comments
+
+# we can use multiline comments line following
+# =begin
+# My
+# multiline
+# comment
+# =end
+
+# but it is not convention
+# convention is to use # on each line
