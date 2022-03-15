4ec059fb1ad92b95eabe5079b54f93a33365a5b6
adandevsinc <adan.shahzad@devsinc.com>
2022-03-16 03:01:23 +0500

    exploring variables

---
+++ variables.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+name = 'adan'
+age = 10
+cpga = 3.21
+is_tall = true
+
+puts "my name is #{name}
+my age is #{age}
+my cgpa is #{cpga}
+is i am tall? #{is_tall}"
+
+name = 'fahad'
+
+print "my brother's name is #{name}"
