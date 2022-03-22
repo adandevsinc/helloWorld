37c022b5897e05f1d86d91d7aca0adbc42e9e9b5
adandevsinc <adan.shahzad@devsinc.com>
2022-03-22 21:31:41 +0500

    Array#product in ruby

---
+++ product.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+CHARACTERS = ["Joey Jeremiah", "Snake Simpson", "Wheels", "Spike Nelson", "Arthur Kobalewscuy", "Caitlin Ryan", "Shane McKay", "Rick Munro", "Stephanie Kaye"]
+
+def degrassi_couples
+  all = CHARACTERS.product(CHARACTERS)
+  all.reject { |c| c.first == c.last }
+end
